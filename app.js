084bace8a197b091cb796b689be17b4caf6c1ba0
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-21 10:52:38 -0500

    Conditional rendering

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goal: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      const parsedGoal = this.goal.trim();
+
+      if (!parsedGoal) {
+        alert("You need to write a Goal!");
+        return;
+      }
+
+      this.goals.push(parsedGoal);
+      this.goal = "";
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
